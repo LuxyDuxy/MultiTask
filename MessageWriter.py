7ed6924068d1be04de9c53e66540becf8c59a4bc
LuxyDuxy <93622772+LuxyDuxy@users.noreply.github.com>
2021-11-20 00:26:52 +0100

    Update MessageWriter.py

--- MessageWriter.py
+++ MessageWriter.py
@@ -2,8 +2,8 @@ import os
 import time
 from typing import NamedTuple
 clear = lambda: os.system("cls")
+clear()
 import socket
-from threading import Thread
 
 print('Enter your name:')
 a = input()
@@ -14,7 +14,7 @@ separator_token = "<SEP>"
 s = socket.socket()
 s.connect((SERVER_HOST, SERVER_PORT))
 clear()
-print('wait...')
+print('Wait...')
 time.sleep(2)
 while True:
     clear()
@@ -22,12 +22,11 @@ while True:
     clear()
     if to_send.lower() == 'q':
         break
-    to_send = f"{a}:{to_send}"
-    print('sending...')
-    time.sleep(2)
+    to_send = f"{a}: {to_send}"
+    print('Sending...')
+    time.sleep(1.5)
     s.send(to_send.encode())
-    time.sleep(1)
     clear()
-    print('sent')
-    time.sleep(2)
+    print('Sent')
+    time.sleep(1)
     clear()
